2bf1366399716fff969a216a002b9edadf0dbe5c
fxbenard <fxbenard@gmail.com>
2016-01-17 19:55:15 +0100

    Change test2 > Test1

--- inc/admin/ui/actions.php
+++ inc/admin/ui/actions.php
@@ -6,9 +6,9 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
  *
  * @since 1.2
  */
-function test_2_action_add_license() { ?>
+function test_1_action_add_license() { ?>
 
-	<div style="display:table-cell; vertical-align:middle; width:20%;"><button type="button" id="test_2_license_activate" class="button-secondary"> <?php _e( 'Activate License', 'test2' ); ?></button><span id="spinner-test-2" class="spinner"></span></div>
+	<div style="display:table-cell; vertical-align:middle; width:20%;"><button type="button" id="test_1_license_activate" class="button-secondary"> <?php _e( 'Activate License', 'test1' ); ?></button><span id="spinner-test-1" class="spinner"></span></div>
 
 <?php }
 
@@ -17,15 +17,15 @@ function test_2_action_add_license() { ?>
  *
  * @since 1.2
  */
-function test_2_action_remove_license( $expires ) {
+function test_1_action_remove_license( $expires ) {
 
 			list( $date, $time ) = explode( " ", $expires );
 			$day_before_expires = ceil(abs( strtotime($date) - time() ) / 86400);
 
 		?>
 
-			<div style="display:table-cell; vertical-align:middle; width:20%;"><span style="color: green;" class="dashicons dashicons-yes"></span> <?php _e( 'License active', 'test2' ); ?></div>
-			<div style="display:table-cell; vertical-align:middle; width:20%; margin-left:2%;"><span class="dashicons dashicons-backup"></span> <?php _e('Expires in', 'test2'); ?> : <strong><?php echo $day_before_expires; ?></strong> <?php _e('days', 'test2'); ?></div>
-			<div style="display:table-cell; vertical-align:middle; width:20%; margin-left:2%;"><button type="button" id="test_2_license_deactivate" class="button-secondary"><span style="vertical-align: middle;" class="dashicons dashicons-no"></span> <?php _e( 'Deactivate License', 'test2' ); ?></button><span id="spinner-test-2" class="spinner"></span></div>
+			<div style="display:table-cell; vertical-align:middle; width:20%;"><span style="color: green;" class="dashicons dashicons-yes"></span> <?php _e( 'License active', 'test1' ); ?></div>
+			<div style="display:table-cell; vertical-align:middle; width:20%; margin-left:2%;"><span class="dashicons dashicons-backup"></span> <?php _e('Expires in', 'test1'); ?> : <strong><?php echo $day_before_expires; ?></strong> <?php _e('days', 'test1'); ?></div>
+			<div style="display:table-cell; vertical-align:middle; width:20%; margin-left:2%;"><button type="button" id="test_2_license_deactivate" class="button-secondary"><span style="vertical-align: middle;" class="dashicons dashicons-no"></span> <?php _e( 'Deactivate License', 'test1' ); ?></button><span id="spinner-test-1" class="spinner"></span></div>
 
 <?php }
