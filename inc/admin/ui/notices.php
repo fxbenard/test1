5c4925f90f6e08462c863d45dee9df0d5d269677
G3ronim0 <jerome@sadler-jerome.fr>
2016-01-21 15:00:34 +0100

    Add new notices and Fix test2 > test1 again

--- inc/admin/ui/notices.php
+++ inc/admin/ui/notices.php
@@ -15,25 +15,42 @@ function test_1_admin_notices() {
 
     switch ( $notice ) {
 
+      case 'expired' :
+        $message_class = 'error';
+        $message = sprintf(
+          __( 'Your license key expired on %s. Please <a href="%s" target="_blank" title="Renew your license key">renew your license key</a>.', 'test1' ),
+          date_i18n( get_option( 'date_format' ), strtotime( $license->expires, current_time( 'timestamp' ) ) ),
+          'https://easydigitaldownloads.com/checkout/?edd_license_key=' . $value . '&utm_campaign=admin&utm_source=licenses&utm_medium=expired'
+        );
+      break;
+
+      case 'missing' :
+        $message_class = 'error';
+        $message = sprintf(
+          __( 'Invalid license. Please <a href="%s" target="_blank" title="Visit account page">visit your account page</a> and verify it.', 'test1' ),
+          'https://easydigitaldownloads.com/your-account?utm_campaign=admin&utm_source=licenses&utm_medium=missing'
+        );
+      break;
+
+      case 'invalid' :
+      case 'site_inactive' :
+        $message_class = 'error';
+        $message = sprintf(
+          __( 'Your %s is not active for this URL. Please <a href="%s" target="_blank" title="Visit account page">visit your account page</a> to manage your license key URLs.', 'test1' ),
+          $args['name'],
+          TEST_1_STORE_URL.'/your-account?utm_campaign=admin&utm_source=licenses&utm_medium=invalid'
+        );
+      break;
+
       case 'item_name_mismatch' :
-  			$message_class = 'error';
-  			$message = __( 'This license does not belong to the product you have entered it for.', 'test1' );
-  			break;
+        $message_class = 'error';
+        $message = sprintf( __( 'This is not a %s.', 'test1' ), $args['name'] );
+      break;
 
-  		case 'no_activations_left' :
-  			$message_class = 'error';
-  			$message = __( 'This license does not have any activations left', 'test1' );
-  			break;
-
-  		case 'expired' :
-  			$message_class = 'error';
-  			$message = __( 'This license key is expired. Please renew it.', 'test1' );
-  			break;
-
-      default :
-				$message_class = 'error';
-				$message = sprintf( __( 'There was a problem activating your license key, please try again or contact support. Error code: %s', 'test1' ), $notice );
-				break;
+      case 'no_activations_left':
+        $message_class = 'error';
+        $message = sprintf( __( 'Your license key has reached its activation limit. <a href="%s">View possible upgrades</a> now.', 'test1' ), 'https://easydigitaldownloads.com/your-account/' );
+      break;
 
   	}
 
