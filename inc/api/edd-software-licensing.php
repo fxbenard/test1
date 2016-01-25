559f7af1ad274f6a48e6b47c4f41efed7e395d80
fxbenard <fxbenard@gmail.com>
2016-01-25 12:28:38 +0100

    wpcs

--- inc/api/edd-software-licensing.php
+++ inc/api/edd-software-licensing.php
@@ -7,7 +7,6 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
  * @since 1.2
  *
  * @param (string) $action (activate_license|check_license|delete_license)
- * @param (string)
  */
 function test_1_edd_software_call( $action, $key ) {
 
@@ -15,36 +14,35 @@ function test_1_edd_software_call( $action, $key ) {
 		$api_params = array(
 			'edd_action' => $action,
 			'license'    => $key,
-			'item_name'  => urlencode( TEST_1_ITEM_NAME ), // the name of our product in EDD
-			'url'        => home_url()
+			'item_name'  => urlencode( TEST_1_ITEM_NAME ), // The name of our product in EDD.
+			'url'        => home_url(),
 		);
 	} else {
 		$api_params = array(
 			'edd_action' => $action,
 			'license'    => $key,
-			'item_name'  => urlencode( TEST_1_ITEM_NAME )
+			'item_name'  => urlencode( TEST_1_ITEM_NAME ),
 		);
 	}
 
-    $args = array(
-      'timeout'   => 30,
-      'sslverify' => false,
-      'body'      => $api_params,
-    );
+	$args = array(
+	  'timeout'   => 30,
+	  'sslverify' => false,
+	  'body'      => $api_params,
+	);
 
-    // Call the custom API.
-    $remote_call = wp_remote_post( add_query_arg( $api_params, TEST_1_STORE_URL ), $args );
+	// Call the custom API.
+	$remote_call = wp_remote_post( add_query_arg( $api_params, TEST_1_STORE_URL ), $args );
 
-    // make sure the response came back okay
-    if ( is_wp_error( $remote_call ) ) {
-			$error_message = sprintf( __('<p class="fxb-error"><span class="dashicons dashicons-info"></span>There is a problem with remote site, please try again. %s</p>', 'test1' ), $remote_call->get_error_message() );
+	// Make sure the response came back okay.
+	if ( is_wp_error( $remote_call ) ) {
+			$error_message = sprintf( __( '<p class="fxb-error"><span class="dashicons dashicons-info"></span>There is a problem with remote site, please try again. %s</p>', 'test1' ), $remote_call->get_error_message() );
 			return $error_message;
-    }else {
-    	// decode the license data
-    	$license_data = json_decode( wp_remote_retrieve_body( $remote_call ) );
-		}
+	} else {
+		// Decode the license data.
+		$license_data = json_decode( wp_remote_retrieve_body( $remote_call ) );
+	}
 
 		return $license_data;
-
 
 }
