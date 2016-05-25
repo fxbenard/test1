dca139dc5b3699af9a7392226d704a1b1a972e48
G3ronim0 <jerome@sadler-jerome.fr>
2016-05-25 15:13:57 +0200

    Prevent a bug when saving options in customizer

--- inc/3rd-party/et-builder.php
+++ inc/3rd-party/et-builder.php
@@ -1,8 +1,10 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
-// Delete LocalStorage on update WP settings.
-add_action( 'updated_option', 'et_pb_force_regenerate_templates', 10, 3 );
+if ( ! is_customize_preview() ) {
+	// Delete LocalStorage on update WP settings
+	add_action( 'updated_option', 'et_pb_force_regenerate_templates', 10, 3 );
+}
 
 // Delete LocalStorage on update Divi options.
 add_action( 'wp_ajax_save_epanel', 'et_pb_force_regenerate_templates' );
